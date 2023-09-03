93b9ea454f9ed2eb2908aa964a58656983de899a
Anders Elmén <anders@need2code.se>
2023-09-03 12:11:28 +0200

    Update SetiScalaDTC.ps1

--- SetiScalaDTC.ps1
+++ SetiScalaDTC.ps1
@@ -19,68 +19,64 @@ if ([string]::IsNullOrWhiteSpace($poolSize)) {
 $defaultTimeout = 3600
 $defaultPoolSize = 1
 
-# Ange sökvägen till loggfilen
-$logFilePath = "C:\Path\To\Log\File.log"
+# CONFIGURE DTC Server
+Set-DtcNetworkSetting -DtcName "Local" -AuthenticationLevel Mutual -InboundTransactionsEnabled $true -OutboundTransactionsEnabled $true -RemoteClientAccessEnabled $true -RemoteAdministrationAccessEnabled $true -XATransactionsEnabled $true -LUTransactionsEnabled $true -Confirm
 
-# Skriv ut till loggfilen
-function Log-Message {
-    param (
-        [string]$message
-    )
-    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-    $logMessage = "$timestamp - $message"
-    $logMessage | Out-File -Append -FilePath $logFilePath
-}
+# CONNECT TO ADMIN
+$comAdmin = New-Object -com ("COMAdmin.COMAdminCatalog.1")
+$LocalColl = $comAdmin.Connect("localhost")
+$LocalComputer = $LocalColl.GetCollection("LocalComputer",$LocalColl.Name)
+$LocalComputer.Populate()
+$Applications =  $LocalColl.GetCollection("Applications", $LocalColl.Name)
+$Applications.Populate()
 
-try {
-    # CONFIGURE DTC Server
-    Set-DtcNetworkSetting -DtcName "Local" -AuthenticationLevel Mutual -InboundTransactionsEnabled $true -OutboundTransactionsEnabled $true -RemoteClientAccessEnabled $true -RemoteAdministrationAccessEnabled $true -XATransactionsEnabled $true -LUTransactionsEnabled $true -Confirm
-
-    # CONNECT TO ADMIN
-    $comAdmin = New-Object -com ("COMAdmin.COMAdminCatalog.1")
-    $LocalColl = $comAdmin.Connect("localhost")
-    $LocalComputer = $LocalColl.GetCollection("LocalComputer",$LocalColl.Name)
-    $LocalComputer.Populate()
-    $Applications =  $LocalColl.GetCollection("Applications", $LocalColl.Name)
-    $Applications.Populate()
-
-    foreach($Application in $Applications)
+# Sätt ConcurrentApps utanför loopen
+foreach ($Application in $Applications)
+{
+    switch ($Application.Name)
     {
-        switch($Application.Name){
-            "Scala Long Calls Host", "Scala Shared State Objects", "Scala Web Components", "Scala Managers", "Scala Database Components", "Scala Business Components", "Scala Short Calls Host" {
-                $Application.Value("ConcurrentApps") = $defaultPoolSize;
-                $Applications.SaveChanges();
+        "Scala Long Calls Host", "Scala Shared State Objects", "Scala Web Components", "Scala Managers", "Scala Database Components", "Scala Business Components", "Scala Short Calls Host"
+        {
+            $Components = $applications.GetCollection("Components", $Application.Key);
+            $Components.Populate();
 
-                $Components = $applications.GetCollection("Components", $Application.Key);
-                $Components.Populate();
-
-                foreach($Component in $Components){
-                    Write-Host $Application.Name, $Component.Name,$Component.Value("ComponentTransactionTimeout")
-                    $Component.Value("ComponentTransactionTimeout") = $defaultTimeout;
-                    Write-Host $Application.Name, $Component.Name,$Component.Value("ComponentTransactionTimeout")
-                }
-
-                $Components.SaveChanges()
+            foreach ($Component in $Components)
+            {
+                Write-Host $Application.Name, $Component.Name, $Component.Value("ComponentTransactionTimeout")
+                $Component.Value("ComponentTransactionTimeout") = $defaultTimeout;
+                Write-Host $Application.Name, $Component.Name, $Component.Value("ComponentTransactionTimeout")
             }
+
+            $Components.SaveChanges()
         }
     }
+}
 
-    $LocalComputerItem = $LocalComputer.Item(0)
+# Sätt ConcurrentApps efter loopen
+foreach ($Application in $Applications)
+{
+    switch ($Application.Name)
+    {
+        "Scala Long Calls Host", "Scala Shared State Objects", "Scala Web Components", "Scala Managers", "Scala Database Components", "Scala Business Components", "Scala Short Calls Host"
+        {
+            $Application.Value("ConcurrentApps") = $defaultPoolSize;
+            $Applications.SaveChanges();
+        }
+    }
+}
+
+$LocalComputerItem = $LocalComputer.Item(0)
+$CurrVal = $LocalComputerItem.Value("TransactionTimeout")
+Write-Host "Transaction Timeout = $CurrVal"
+$LocalComputerItem.Value("TransactionTimeout") = $timeout
+$result = $LocalComputer.SaveChanges()
+if ($result.Equals(1))
+{
     $CurrVal = $LocalComputerItem.Value("TransactionTimeout")
     Write-Host "Transaction Timeout = $CurrVal"
-    $LocalComputerItem.Value("TransactionTimeout") = $timeout
-    $result = $LocalComputer.SaveChanges()
-    if($result.Equals(1)){
-        $CurrVal = $LocalComputerItem.Value("TransactionTimeout")
-        Write-Host "Transaction Timeout = $CurrVal"
-    }
-
-    Log-Message "Scriptet kördes utan fel."
-} catch {
-    $errorMessage = "Ett fel inträffade: $($_.Exception.Message)"
-    Write-Host $errorMessage -ForegroundColor Red
-    Log-Message $errorMessage
 }
+
+Log-Message "Scriptet kördes utan fel."
 
 # Visa meddelande om att bjuda på kaffe
 $phoneNumber = "0735191031"
